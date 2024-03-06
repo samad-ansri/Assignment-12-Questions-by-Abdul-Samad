c5dc3a1728155f12e3238763912bd3e31de6d45c
samad-ansri <159898104+samad-ansri@users.noreply.github.com>
2024-03-06 23:54:57 +0500

    Add files via upload

---
+++ q10.js
@@ -0,0 +1,4 @@
+var names = ["ali", "ahmed", "noman"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
---
+++ q11.js
@@ -0,0 +1,5 @@
+var names = ["ali", "ahmed", "noman"];
+var message = "would you like to learn typescript ";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
