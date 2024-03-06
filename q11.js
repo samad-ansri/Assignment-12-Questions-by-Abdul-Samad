5a2a3ba9cb8c276617e76c88969920a16bcf7200
samad-ansri <159898104+samad-ansri@users.noreply.github.com>
2024-03-06 23:53:27 +0500

    Delete q11.js

--- q11.js
+++
@@ -1,5 +0,0 @@
-var names = ["ali", "ahmed", "noman"];
-var message = "would you like to learn typescript ";
-console.log(names[0] + " " + message);
-console.log(names[1] + " " + message);
-console.log(names[2] + " " + message);
